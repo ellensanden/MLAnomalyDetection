132b8e7efe9ef1691a125e90921802bee14a4a67
ellensanden <53423025+ellensanden@users.noreply.github.com>
2021-04-23 10:13:09 +0200

    updata

--- test_stuff/overlapping_window.py
+++ test_stuff/overlapping_window.py
@@ -1,7 +1,8 @@
 
 
 def overlap_window (window_size,overlap,seq): # overlap 1 is max. larger number would be less overlap
- 
+    import numpy as np
+    from numpy import array
     seq = array([seq[i : i + window_size] for i in range(0, len(seq), overlap)]) 
    
     correct = [len(x)==window_size for x in seq]
