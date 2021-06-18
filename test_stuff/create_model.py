eeb8de3e04df12f1e74a2ece850c9f135af9927f
ellensanden <53423025+ellensanden@users.noreply.github.com>
2021-06-18 14:15:45 +0200

    df

--- test_stuff/create_model.py
+++ test_stuff/create_model.py
@@ -23,42 +23,61 @@ def make_model(type):
 
     if type == 'bi_convLSTM':
     
-        input = Input(shape=(40,64,17,1))
+        # input = Input(shape=(40,64,17,1))
 
-        x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (11, 9),strides=(2, 2),return_sequences=True,return_state=False),merge_mode='sum')(input)
+        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (11, 9),strides=(2, 2),return_sequences=True,return_state=False),merge_mode='sum')(input)
 
-        x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True), merge_mode='sum')(x)
+        # x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True), merge_mode='sum')(x)
 
-        x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True),  merge_mode='sum')(x, initial_state = [state_h1,state_c1, state_h2, state_c2])
+        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True),  merge_mode='sum')(x, initial_state = [state_h1,state_c1, state_h2, state_c2])
 
-        x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (9, 3),strides=(2, 1),return_sequences=True,return_state=False),merge_mode='sum')(x)
+        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (9, 3),strides=(2, 1),return_sequences=True,return_state=False),merge_mode='sum')(x)
 
-        x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True),merge_mode='sum')(x)
+        # x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True),merge_mode='sum')(x)
 
-        x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True),merge_mode='sum' )(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
+        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True),merge_mode='sum' )(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
 
-        x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (3, 1),strides=(1, 1),return_sequences=True),merge_mode='sum')(x)
+        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (3, 1),strides=(1, 1),return_sequences=True),merge_mode='sum')(x)
 
-        x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True), merge_mode = 'sum')(x)
+        # x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True), merge_mode = 'sum')(x)
 
-        x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True), merge_mode='sum')(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
+        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True), merge_mode='sum')(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
 
 
-        k1 = 40 - x.shape[1] + 1 
+        # k1 = 40 - x.shape[1] + 1 
+        # k2 = 64 - x.shape[2] + 1
+        # k3 = 17 - x.shape[3] + 1 
+
+        # x = Conv3DTranspose(1,kernel_size=(k1,k2,k3), strides=(1, 1, 1))(x) # this should work always as long as strides are 1,1,1
+        # x = TimeDistributed(Dense(1, activation='sigmoid'))(x)
+
+        # CNN = Model(inputs=input, outputs=x,name="CNN")
+        # CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
+        # CNN.summary()
+
+        # model = CNN
+        # return model
+        # Bidirectional on same shape as convlstm
+        input = Input(shape=(40 ,64,17,1))
+
+        x = Bidirectional(ConvLSTM2D(filters = 5, kernel_size = (11, 9),strides=(2, 2),return_sequences=True))(input)
+        x = Bidirectional(ConvLSTM2D(filters = 5, kernel_size = (9, 3),strides=(2, 2),return_sequences=True))(x)
+        x = Bidirectional(ConvLSTM2D(filters = 5, kernel_size = (3, 1),strides=(2, 1),return_sequences=True))(x)
+        x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 5, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True))(x)
+        x = Bidirectional(ConvLSTM2D(filters = 5, kernel_size = (1, 1),strides=(1, 1),return_sequences=True))(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
+
+        k1 = 40  - x.shape[1] + 1 
         k2 = 64 - x.shape[2] + 1
         k3 = 17 - x.shape[3] + 1 
 
-        x = Conv3DTranspose(1,kernel_size=(k1,k2,k3), strides=(1, 1, 1))(x) # this should work always as long as strides are 1,1,1
-        x = TimeDistributed(Dense(1, activation='sigmoid'))(x)
-
+        x = Conv3DTranspose(1,kernel_size=(k1,k2,k3), strides=(1, 1, 1),activation='relu')(x) 
         CNN = Model(inputs=input, outputs=x,name="CNN")
         CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
         CNN.summary()
-
         model = CNN
         return model
-    #return model
-    elif type == 'cannolo_LSTM':
+
+    elif type == 'small_LSTM':
         lstm_initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
 
         # define Encoder
@@ -81,8 +100,31 @@ def make_model(type):
 
         model = EncoderDecoder
 
-        #return model
+        return model
+    elif type == 'cannolo_LSTM':
+            lstm_initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
 
+            # define Encoder
+            Inputs = Input(shape=(40,64))
+            dense1 =Dense(256, activation='tanh')(Inputs)
+            dropout = Dropout(0.2)(dense1)
+            lstm1 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(dropout)
+            lstm2, state_h, state_c = LSTM(128,return_sequences=True,return_state=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm1)
+            encoder_states = [state_h, state_c]
+
+            # define Decoder
+            lstm3 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm2,initial_state=encoder_states)
+            lstm4 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm3)
+            dense2 = Dense(256, activation='sigmoid')(lstm4)
+            output = Dense(64,activation= 'sigmoid')(dense2)
+
+            EncoderDecoder = Model(inputs=Inputs, outputs=output,name="EncoderDecoder")
+            EncoderDecoder.compile(optimizer='adam', loss='BinaryCrossentropy')
+            EncoderDecoder.summary()
+
+            model = EncoderDecoder
+
+            return model
     elif type == '3dCNN':
         input = Input(shape=(40,64,17,1))
 
@@ -181,7 +223,7 @@ def make_model(type):
         x = LSTM(64,return_sequences = True)(x)
         x,h,c = LSTM(64,return_sequences = True,return_state=True)(x)
         x = LSTM(64,return_sequences = True)(x,initial_state=[h,c])
-        x = LSTM(128, return_sequences = True)(x)
+        x = LSTM(128, return_sequences = True)(x) 
 
         x = TimeDistributed(Reshape((64,2,1)))(x)
         x = TimeDistributed(Conv2DTranspose(filters = 5, kernel_size = (1, 5), activation='relu', padding='valid',strides=(1,1)))(x)
