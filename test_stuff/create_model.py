ad0b1bec0110386df801289d72bbd28806147871
ellensanden <53423025+ellensanden@users.noreply.github.com>
2021-06-24 16:26:27 +0200

    all

--- test_stuff/create_model.py
+++ test_stuff/create_model.py
@@ -17,47 +17,11 @@ def make_model(type):
     from keras.layers import Reshape
     from keras.layers import MaxPool2D
     from keras.layers import Conv2DTranspose
-    from keras.layers import Permute
 
     print(type)
 
     if type == 'bi_convLSTM':
-    
-        # input = Input(shape=(40,64,17,1))
 
-        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (11, 9),strides=(2, 2),return_sequences=True,return_state=False),merge_mode='sum')(input)
-
-        # x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True), merge_mode='sum')(x)
-
-        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True),  merge_mode='sum')(x, initial_state = [state_h1,state_c1, state_h2, state_c2])
-
-        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (9, 3),strides=(2, 1),return_sequences=True,return_state=False),merge_mode='sum')(x)
-
-        # x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True),merge_mode='sum')(x)
-
-        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True),merge_mode='sum' )(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
-
-        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (3, 1),strides=(1, 1),return_sequences=True),merge_mode='sum')(x)
-
-        # x, state_h1, state_c1, state_h2, state_c2 = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True,return_state=True), merge_mode = 'sum')(x)
-
-        # x = Bidirectional(ConvLSTM2D(filters = 2, kernel_size = (1, 1),strides=(1, 1),return_sequences=True), merge_mode='sum')(x, initial_state = [state_h1, state_c1, state_h2, state_c2])
-
-
-        # k1 = 40 - x.shape[1] + 1 
-        # k2 = 64 - x.shape[2] + 1
-        # k3 = 17 - x.shape[3] + 1 
-
-        # x = Conv3DTranspose(1,kernel_size=(k1,k2,k3), strides=(1, 1, 1))(x) # this should work always as long as strides are 1,1,1
-        # x = TimeDistributed(Dense(1, activation='sigmoid'))(x)
-
-        # CNN = Model(inputs=input, outputs=x,name="CNN")
-        # CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
-        # CNN.summary()
-
-        # model = CNN
-        # return model
-        # Bidirectional on same shape as convlstm
         input = Input(shape=(40 ,64,17,1))
 
         x = Bidirectional(ConvLSTM2D(filters = 5, kernel_size = (11, 9),strides=(2, 2),return_sequences=True))(input)
@@ -71,16 +35,13 @@ def make_model(type):
         k3 = 17 - x.shape[3] + 1 
 
         x = Conv3DTranspose(1,kernel_size=(k1,k2,k3), strides=(1, 1, 1),activation='relu')(x) 
-        CNN = Model(inputs=input, outputs=x,name="CNN")
-        CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
-        CNN.summary()
-        model = CNN
-        return model
-
+        model = Model(inputs=input, outputs=x,name="bi_convLSTM")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
+       
     elif type == 'small_LSTM':
         lstm_initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
 
-        # define Encoder
         Inputs = Input(shape=(40,64))
         dense1 =Dense(256, activation='tanh')(Inputs)
         dropout = Dropout(0.2)(dense1)
@@ -88,75 +49,64 @@ def make_model(type):
         lstm2, state_h, state_c = LSTM(40,return_sequences=True,return_state=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm1)
         encoder_states = [state_h, state_c]
 
-        # define Decoder
         lstm3 = LSTM(40,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm2,initial_state=encoder_states)
         lstm4 = LSTM(40,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm3)
         dense2 = Dense(256, activation='sigmoid')(lstm4)
         output = Dense(64,activation= 'sigmoid')(dense2)
 
-        EncoderDecoder = Model(inputs=Inputs, outputs=output,name="EncoderDecoder")
-        EncoderDecoder.compile(optimizer='adam', loss='BinaryCrossentropy')
-        EncoderDecoder.summary()
+        model = Model(inputs=Inputs, outputs=output,name="small_LSTM")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
 
-        model = EncoderDecoder
+    elif type == 'large_LSTM':
+        lstm_initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
 
-        return model
-    elif type == 'cannolo_LSTM':
-            lstm_initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
+        Inputs = Input(shape=(40,64))
+        dense1 =Dense(256, activation='tanh')(Inputs)
+        dropout = Dropout(0.2)(dense1)
+        lstm1 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(dropout)
+        lstm2, state_h, state_c = LSTM(128,return_sequences=True,return_state=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm1)
+        encoder_states = [state_h, state_c]
 
-            # define Encoder
-            Inputs = Input(shape=(40,64))
-            dense1 =Dense(256, activation='tanh')(Inputs)
-            dropout = Dropout(0.2)(dense1)
-            lstm1 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(dropout)
-            lstm2, state_h, state_c = LSTM(128,return_sequences=True,return_state=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm1)
-            encoder_states = [state_h, state_c]
+        lstm3 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm2,initial_state=encoder_states)
+        lstm4 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm3)
+        dense2 = Dense(256, activation='sigmoid')(lstm4)
+        output = Dense(64,activation= 'sigmoid')(dense2)
 
-            # define Decoder
-            lstm3 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm2,initial_state=encoder_states)
-            lstm4 = LSTM(128,return_sequences=True,kernel_initializer =lstm_initializer, recurrent_initializer=lstm_initializer)(lstm3)
-            dense2 = Dense(256, activation='sigmoid')(lstm4)
-            output = Dense(64,activation= 'sigmoid')(dense2)
+        model = Model(inputs=Inputs, outputs=output,name="large_LSTM")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
 
-            EncoderDecoder = Model(inputs=Inputs, outputs=output,name="EncoderDecoder")
-            EncoderDecoder.compile(optimizer='adam', loss='BinaryCrossentropy')
-            EncoderDecoder.summary()
-
-            model = EncoderDecoder
-
-            return model
     elif type == '3dCNN':
         input = Input(shape=(40,64,17,1))
 
-        x = Conv3D(filters = 5, kernel_size = (21, 41, 9), activation='relu', strides=(1, 1, 1), padding='same')(input) # A
-        x = MaxPool3D((2,2,2),padding='same')(x)                                                                        # B
+        x = Conv3D(filters = 5, kernel_size = (21, 41, 9), activation='relu', strides=(1, 1, 1), padding='same')(input) 
+        x = MaxPool3D((2,2,2),padding='same')(x)                                                                        
 
-        x = Conv3D(filters = 5, kernel_size = (11, 21, 3),  activation='relu', strides=(1, 1, 1), padding='same')(x)    # C
-        x = MaxPool3D((2,2,2),padding='same')(x)                                                                        # B
+        x = Conv3D(filters = 5, kernel_size = (11, 21, 3),  activation='relu', strides=(1, 1, 1), padding='same')(x)    
+        x = MaxPool3D((2,2,2),padding='same')(x)                                                                        
 
-        x = Conv3D(filters = 5, kernel_size = (3, 3, 3), strides=(1, 1, 1), activation='relu', padding='same')(x)       # D
-        x = MaxPool3D((1,1,1),padding='same')(x)                                                                        # E
+        x = Conv3D(filters = 5, kernel_size = (3, 3, 3), strides=(1, 1, 1), activation='relu', padding='same')(x)       
+        x = MaxPool3D((1,1,1),padding='same')(x)                                                                        
 
-        x = Conv3D(filters = 5, kernel_size = (9, 9, 3), strides=(1, 1, 1), padding='same')(x)  # this is in the wrong spot ??? F
-        x = BatchNormalization()(x)                                                                                     # G
+        x = Conv3D(filters = 5, kernel_size = (9, 9, 3), strides=(1, 1, 1), padding='same')(x)  
+        x = BatchNormalization()(x)                                                                                     
         x = Activation('relu')(x)                                                                                        
-        x = MaxPool3D((1,1,1),padding='same')(x)                                                                         # E
+        x = MaxPool3D((1,1,1),padding='same')(x)                                                                        
 
+        x = Conv3DTranspose(5,kernel_size=(9,9,3), strides=(1, 1, 1),activation='relu')(x)                              
 
-        x = Conv3DTranspose(5,kernel_size=(9,9,3), strides=(1, 1, 1),activation='relu')(x)                              # H
-
-        x = Conv3DTranspose(5,kernel_size=(11,11,11), strides=(1, 1, 1),activation='relu')(x)                              #I 
+        x = Conv3DTranspose(5,kernel_size=(11,11,11), strides=(1, 1, 1),activation='relu')(x)                               
 
         k1 = 40 - x.shape[1] + 1
         k2 = 64 - x.shape[2] + 1
         k3 = 17 - x.shape[3] + 1 
 
-        x = Conv3DTranspose(5,kernel_size=(k1,k2,k3), strides=(1, 1, 1),activation='relu')(x)                           #J
-        x = Dense(1,activation = 'sigmoid')(x)                                                                          #K    
-        CNN = Model(inputs=input, outputs=x,name="CNN")
-        CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
-        CNN.summary()
-        model = CNN
+        x = Conv3DTranspose(5,kernel_size=(k1,k2,k3), strides=(1, 1, 1),activation='relu')(x)                           
+        x = Dense(1,activation = 'sigmoid')(x)                                                                             
+        model = Model(inputs=input, outputs=x,name="3dCNN")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
 
     elif type =='cnn_lstm':
         input = Input(shape=(64,17,40))
@@ -183,11 +133,9 @@ def make_model(type):
         x = Conv2DTranspose(filters = 40, kernel_size = (1, 12), activation='relu', padding='valid',strides=(1,1))(x)
         x = Dense(40,activation='sigmoid')(x)
 
-        CNN = Model(inputs=input, outputs=x,name="CNN")
-        CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
-        CNN.summary()
-
-        model = CNN
+        model = Model(inputs=input, outputs=x,name="2d_cnn_lstm")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
 
     elif type=='conv-lstm':
         input = Input(shape=(40 ,64,17,1))
@@ -203,11 +151,10 @@ def make_model(type):
         k3 = 17 - x.shape[3] + 1 
 
         x = Conv3DTranspose(1,kernel_size=(k1,k2,k3), strides=(1, 1, 1),activation='sigmoid')(x) 
-        CNN = Model(inputs=input, outputs=x,name="CNN")
-        CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
-        CNN.summary()
+        model = Model(inputs=input, outputs=x,name="conv-lstm")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
 
-        model = CNN
     elif type == 'timedist_cnn_lstm':
         input = Input(shape=(40, 64, 17, 1))
 
@@ -230,11 +177,10 @@ def make_model(type):
         x = TimeDistributed(Conv2DTranspose(filters = 5, kernel_size = (1, 12), activation='relu', padding='valid',strides=(1,1)))(x)
         x = TimeDistributed(Dense(1,activation='sigmoid'))(x)
 
-        CNN = Model(inputs=input, outputs=x,name="CNN")
-        CNN.compile(optimizer='adam', loss='BinaryCrossentropy')
-        CNN.summary()
+        model = Model(inputs=input, outputs=x,name="timedist_cnn_lstm")
+        model.compile(optimizer='adam', loss='BinaryCrossentropy')
+        model.summary()
 
-        model = CNN   
     else:
         model = []
         print('wrong type name') 
--- test_stuff/run_test_data.py
+++ test_stuff/run_test_data.py
@@ -1,50 +1,40 @@
 import pandas as pd
-import tensorflow as tf
 import keras
 import pickle
-from keras.callbacks import EarlyStopping
 import numpy as np
-from cont_data_process import continuous_process
-from prepare_data_cube_cont import make_cubes_cont
+from prepare_data_cube import make_cubes
 from prepare_LSTM_data import LSTM_data
-from create_model import make_model
 
-filename = 'dataframe_test_attack.csv'
-#filename = 'gear_test.csv'
+# the data set to test on
+filename = 'gear_test.csv'
+
 df = pd.read_csv(filename, sep=',')
-#allRows = df.shape[0]
-allRows = 148400
+allRows = df.shape[0]
+
 IDs = df['ID']
 IDs = np.array(IDs)
-IDs = IDs[:148400]
-#attack = df[df['Attack'] == 'T'].copy()
-attack = df[df['Attack'] == 'Yes'].copy()
-attack = attack[:148400]
+
+attack = df[df['Attack'] == 'T'].copy() # this is 'Yes' instead of 'T' for some data sets
 attack_ind = attack.index
 
+#there will be different column names to drop here depending on data set
 dataValues = df.drop([ "ID", "Packet Deltatime", "Attack","Attack Window Number", "Normal Window Number"], axis = 1).copy()
-#dataValues = df.drop([ "Timestamp","ID", "Packet Deltatime", "Attack","Attack Window Number", "Normal Window Number","Total Time","Dataset"], axis = 1).copy()
 
 dataValues = dataValues.to_numpy() 
 print(dataValues.shape)
-dataValues = dataValues[:148400]
-#n_steps = int(allRows/10000) # about 10k in each step #127 before
-n_steps = int(allRows/14840) 
+
+n_steps = int(allRows/10000) # about 10k in each step 
 print(n_steps)
 split = np.array_split(range(allRows), n_steps)
 
-#type = 'cnn'
-#type = 'lstm'
-#modelname = '3dCNN_final'
-#modelname = 'small_LSTM_final'
-type = 'cnn_lstm'
-modelname = '2d_cnn_lstm_new_dataset'
+type = 'cnn_lstm' # type of the trained model
+modelname = 'trained_model' # the name of the trained model
 
 model =  keras.models.load_model(modelname)
 
 all_attack_samples = []
 errors = []
-overlap = 40
+overlap = 40 # overlap = 40 means no overlap 
 for y in range(n_steps):
     print(f'{y} of {n_steps}')
     data_ind = split[y]
@@ -52,9 +42,12 @@ for y in range(n_steps):
     t_IDs = IDs[data_ind]
     training_data = dataValues[data_ind,:]
 
-    x_val,samples = make_cubes_cont(t_IDs,training_data,40,type,data_ind,overlap) 
-    #x_val,samples = LSTM_data(training_data,40,data_ind)
+    if type == 'small_lstm' or type == 'large_lstm':
+        x_val,samples = LSTM_data(training_data,overlap,data_ind)
 
+    else:  
+        x_val,samples = make_cubes(t_IDs,training_data,40,type,data_ind,overlap) 
+    
     contains_attack = [np.any(np.in1d(x, attack_ind)) for x in samples]
     attack_samples = contains_attack
     
@@ -62,14 +55,14 @@ for y in range(n_steps):
     yHat_normal = model.predict(x_val) # only normal packets
     all_errors = x_val-yHat_normal
 
-    if type == 'timeDist_cnn' or type == 'cnn':
-        all_errors = all_errors.reshape(all_errors.shape[0],all_errors.shape[1]*all_errors.shape[2]*all_errors.shape[3]*all_errors.shape[4])
+    if type == 'large_lstm' or type == 'small_lstm':
+        all_errors = all_errors.reshape(all_errors.shape[0],all_errors.shape[1]*all_errors.shape[2])
 
-    if type == 'cnn_lstm':
+    elif type == '2d_cnn_lstm':
         all_errors = all_errors.reshape(all_errors.shape[0],all_errors.shape[1]*all_errors.shape[2]*all_errors.shape[3])
     
-    if type == 'lstm':
-        all_errors = all_errors.reshape(all_errors.shape[0],all_errors.shape[1]*all_errors.shape[2])
+    else:
+        all_errors = all_errors.reshape(all_errors.shape[0],all_errors.shape[1]*all_errors.shape[2]*all_errors.shape[3]*all_errors.shape[4])
 
     all_errors = np.abs(all_errors)
     all_errors = np.sum(all_errors,axis=1)
@@ -77,55 +70,14 @@ for y in range(n_steps):
     
     all_attack_samples.append(attack_samples)
  
- 
-f = open('test_errors_2d_cnn_lstm_new_dataset_06-21.pckl', 'wb')
+saved_test_errors = 'test_errors.pckl' # name of the saved test errors
+f = open(saved_test_errors, 'wb')
 pickle.dump(errors, f)
 f.close()
 
-
-f = open('all_attack_samples_2d_cnn_lstm_new_dataset_06-21.pckl', 'wb')
+saved_true_attack_samples = 'attack_samples.pckl' # name of the saved true attack samples (for later evaluation)
+f = open(saved_true_attack_samples, 'wb')
 pickle.dump(all_attack_samples, f)
 f.close()
 
-# import pickle
-# import keras
-# import tensorflow as tf
 
-# from keras.callbacks import EarlyStopping
-# import numpy as np
-# import pandas as pd 
-
-# n_timesteps = 40
-
-# filename = 'dataframe_test_attack.csv'
-# df = pd.read_csv(filename, sep=',')
-# allRows = df.shape[0]
-
-# IDs = df['ID']
-# attack = df[df['Attack'] == 'Yes'].copy()
-# attack_ind = attack.index
-
-# dataValues = df.drop([ "ID", "Packet Deltatime", "Attack","Attack Window Number", "Normal Window Number"], axis = 1).copy()
-
-# dataValues = dataValues.to_numpy() 
-
-# from prepare_data_cube import make_cubes
-
-# #type = 'timeDist_cnn'
-# type = 'cnn_lstm'
-
-# x_test,x_train, samples = make_cubes(IDs,dataValues,n_timesteps,type)
-
-
-# contains_attack = [np.any(np.in1d(x, attack_ind)) for x in samples]
-
-# normal_errors = x_test-yHat_normal
-
-# if type == 'timeDist_cnn' or type == 'cnn':
-#     normal_errors = normal_errors.reshape(normal_errors.shape[0],normal_errors.shape[1]*normal_errors.shape[2]*normal_errors.shape[3]*normal_errors.shape[4])
-
-# if type == 'cnn_lstm':
-#     normal_errors = normal_errors.reshape(normal_errors.shape[0],normal_errors.shape[1]*normal_errors.shape[2]*normal_errors.shape[3])
-
-# normal_errors = np.abs(normal_errors)
-#normal_errors = np.sum(normal_errors,axis=1)
